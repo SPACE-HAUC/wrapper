3cc16cdd33379e1231b4337a13d4000c908ccda0
Sean <sjc625@comcast.net>
2019-04-13 21:09:57 -0400

    Update cmake

---
+++ CMakeFiles/wrapper.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/wrapper/src/wrapper.cpp" "/home/pi/wrapper/CMakeFiles/wrapper.dir/src/wrapper.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "include/api"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/wrapper.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wrapper.dir/src/wrapper.cpp.o"
+  "bin/wrapper.pdb"
+  "bin/wrapper"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wrapper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
